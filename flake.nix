25cd57689653bc0558d6d1f83a9cee9f69f5cfbb
Cristian Oliveira <contato@cristianoliveira.com.br>
2024-07-08 22:58:45 +0200

    refact(nix): clean up and default pckg (#8)
    * chore(nix): use absolute path for scripts
    
    * flake with utils
    
    * logs
    
    * shell
    
    * chore(nix): adds dev shell
    
    * chore(nix): adds default pckg
    
    * lock

--- flake.nix
+++ flake.nix
@@ -9,55 +9,52 @@
       let
         pkgs = import nixpkgs { inherit system; };
         shell = pkgs.bash;
-      in {
 
+        pluginPkgs = with pkgs; [
+          (lua5_2.withPackages (ps: with ps; [
+            busted 
+            luafilesystem
+            luacheck 
+            luarocks
+          ]))
+          neovim
+        ];
+
+        funzzy_nvim = pkgs.stdenv.mkDerivation {
+          name = "fzz vim for ${system}";
+          src = ./.;
+          doCheck = true;
+
+          checkInputs = pluginPkgs;
+
+          # NOTE: Configure the test environment
+          configurePhase = ''
+            export NVIM_BIN=${pkgs.neovim}/bin/nvim
+          '';
+
+          # NOTE: Run the tests
+          checkPhase = ''
+
+            echo "Running quick checks:"
+            ${pkgs.lua54Packages.busted}/bin/busted lua
+            ${pkgs.lua54Packages.luacheck}/bin/luacheck lua
+
+            echo "Running tests: for ${system}"
+            ${shell}/bin/bash ./nvim-test.sh
+          '';
+
+          buildPhase = ''
+            echo "Building plugins for ${system}"
+          '';
+        };
+      in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            hello
-          ];
+          packages = pluginPkgs;
         };
 
         packages = {
-          default = pkgs.hello;
-          funzzy_nvim = pkgs.stdenv.mkDerivation {
-            name = "fzz vim for ${system}";
-            src = ./.;
-            doCheck = true;
-
-            checkInputs = with pkgs; [
-              (pkgs.lua5_2.withPackages (ps: with ps; [
-                busted 
-                luafilesystem
-                luacheck 
-                luarocks
-              ]))
-              neovim
-
-            ];
-
-            # NOTE: Configure the test environment
-            configurePhase = ''
-              export NVIM_BIN=${pkgs.neovim}/bin/nvim
-            '';
-
-            # NOTE: Run the tests
-            checkPhase = ''
-
-              echo "Running quick checks:"
-              ${pkgs.lua54Packages.busted}/bin/busted lua
-              ${pkgs.lua54Packages.luacheck}/bin/luacheck lua
-
-              ls -la
-
-              echo "Running tests: for ${system}"
-              ${shell}/bin/bash ./nvim-test.sh
-            '';
-
-            buildPhase = ''
-              echo "Building plugins for ${system}"
-              
-            '';
-          };
+          default = funzzy_nvim;
+          funzzy_nvim = funzzy_nvim;
         };
     });
 }
